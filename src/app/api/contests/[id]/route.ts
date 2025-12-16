5b67bbce09387e1e6048ed5e9cec6f9075943d67
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2025-12-16 11:13:45 -0300

    :wrench: chore: Correção de parametros do Delete da API.

--- src/app/api/contests/[id]/route.ts
+++ src/app/api/contests/[id]/route.ts
@@ -6,6 +6,7 @@ import slugify from '@/schemas/slugify.schema';
 import { Prisma } from '../../../../../generated/prisma/client';
 
 export async function DELETE(
+  req: Request,
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
