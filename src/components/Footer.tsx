a58108ca7aff1379a190e0f6dd0713071b4850a9
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-05-11 13:02:40 -0300

    :bookmark: Atualização de versão do sistema no footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className='w-full my-6 flex justify-center'>
-        <span className='text-sm text-muted-foreground'>Desenvolvido por <strong>João Guilherme</strong> &copy; {new Date().getFullYear()} - v2.0.1</span>
+      <span className='text-sm text-muted-foreground'>Desenvolvido por <strong>João Guilherme</strong> &copy; {new Date().getFullYear()} - v2.1.3</span>
     </footer>
   );
 }
